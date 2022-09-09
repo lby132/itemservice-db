30471cbd3a1c35263019c711d58131aa10306336
pro <quddbscjswow@naver.com>
2022-09-09 20:33:17 +0900

    jpa, query dsl 사용 crud

--- src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -1,17 +1,27 @@
 package hello.itemservice.repository.jpa;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.itemservice.domain.Item;
+import hello.itemservice.domain.QItem;
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 import java.util.Optional;
 
+import static hello.itemservice.domain.QItem.*;
+
 @Repository
+@RequiredArgsConstructor
+@Transactional
 public class JpaItemRepositoryV3 implements ItemRepository {
 
     private final EntityManager em;
@@ -24,21 +34,41 @@ public class JpaItemRepositoryV3 implements ItemRepository {
 
     @Override
     public Item save(Item item) {
-        return null;
+        em.persist(item);
+        return item;
     }
 
     @Override
     public void update(Long itemId, ItemUpdateDto updateParam) {
-
+        Item findItem = em.find(Item.class, itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
     }
 
     @Override
     public Optional<Item> findById(Long id) {
-        return Optional.empty();
+        Item item = em.find(Item.class, id);
+        return Optional.ofNullable(item);
     }
 
     @Override
     public List<Item> findAll(ItemSearchCond cond) {
-        return null;
+
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (StringUtils.hasText(itemName)) {
+            builder.and(item.itemName.like("%" + itemName + "%"));
+        }
+        if (maxPrice != null) {
+            builder.and(item.price.loe(maxPrice));
+        }
+
+        return query.select(item)
+                .from(item)
+                .where()
+                .fetch();
     }
 }
